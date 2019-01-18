1da2eb520e5067612a6869ce6296ae4590c83d80
uvural <umutcan.vural@gmail.com>
2019-01-18 13:11:31 +0100

    version 0.1- basic integration

---
+++ Assets/Plugins/Fingercountdeneme.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+
+namespace Leap.Unity{
+
+	public class Fingercountdeneme : MonoBehaviour
+	{
+	
+		
+		
+	}
+}
---
+++ Assets/Plugins/PrintMessage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Leap;
+using Leap.Unity;
+
+public class PrintMessage : MonoBehaviour
+{
+
+	public string Handname;
+
+	public void PrintActivateMessage()
+	{
+		print("Activated");
+		// if ( activated hand is this ){
+		// count}
+		// else if (){}
+		
+	}
+    
+	public void PrintDeactivateMessage()
+	{
+		print("Deactivated");
+	}
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		PrintHandName(Handname);
+		
+	}
+	
+	// Update is called once per frame
+	void Update (){
+	
+	}
+
+	void PrintHandName(string Handname)
+	{
+		print("Current hand is: " + Handname);
+	}
+	
+	
+	void OnEnable()
+	{
+
+		print("inside OnEnable");
+	}
+
+	void OnDisable(){
+		print("inside OnDisable");
+	}
+	
+	
+
+	
+}
+
+
