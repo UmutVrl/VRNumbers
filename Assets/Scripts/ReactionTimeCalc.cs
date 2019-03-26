e402f49e223fa9194feaa1cf99738eaf349571ca
uvural <umutcan.vural@gmail.com>
2019-03-26 17:49:45 +0100

    DigitDetector added

--- Assets/Scripts/ReactionTimeCalc.cs
+++ Assets/Scripts/ReactionTimeCalc.cs
@@ -13,17 +13,19 @@ using Debug = UnityEngine.Debug;
 
 public class ReactionTimeCalc : MonoBehaviour {
 
+	
 	public  float reactionTimer;
 	public  bool isTiming = false;
-
 	public  ExtendedFingerDetector Other;
 
-	private int Condition01;
 	
+//	int fingercount;
+	
+//	private bool Durum = false;
 	
 	
 	//private int[] handnumber = new int[10];
-	string[] digits = new string[5];
+		 string[] digits = new string[5];
 	
 	// Use this for initialization
 	
@@ -45,75 +47,89 @@ public class ReactionTimeCalc : MonoBehaviour {
 		
 		IEnumerator  Bekleme()
 		{
+			
 			yield return new WaitForSeconds(2.5f);
 			GetComponent<Renderer>().material.color = Color.green;
 		//	Debug.Log("Reaction time: "+reactionTimer+"ms");
 			yield return new WaitForSeconds(0.5f);
 
 			int randomNumber = Random.Range(0, 4);
-			
+
 			GameObject.Find("Numerator").GetComponent<TextMesh>().text = digits[randomNumber];
 			
-		//	Debug.Log("Reaction time: "+reactionTimer+"ms");
+			DigitDetector();
+			
+		//	int response = 0;
+
+			
+/*
+			
+			for (int i = 0; response != 1 && response != 2 && response != 3 && response != 4 && response != 5; i++)
+			{
+				
+				Condition01();
+				yield return new WaitForSeconds(0.10f);
+				Condition02();
+				yield return new WaitForSeconds(0.10f);
+				Condition03();
+				yield return new WaitForSeconds(0.10f);
+				Condition04();
+				yield return new WaitForSeconds(0.10f);
+				Condition05();
+				yield return new WaitForSeconds(0.10f);
+				print(fingercount);
+				response = fingercount;
+				
+			}
+
+*/
+
+			//	Debug.Log("Reaction time: "+reactionTimer+"ms");
 			//Show digit
 		}
 
 
-/*
-	void DigitDetector()
+	void DigitDetector() //Hand Digit Calculator
 	{
-		if ()
-		{
-			print("1");
+		//BOTH hands are active. LOOK
+		
+		int a = 0,b = 0,c = 0,d = 0,e = 0,toplam = 0; 
+		
+		Leap.Hand hand;
+		hand = Other.HandModel.GetLeapHand();
 
-		} else if ()
-		{
-			print("2");
-		}
-		else
-		{
-			print("NO");
-		}
+		if (hand.Fingers[0].IsExtended){ a = 1;}
+		if (hand.Fingers[1].IsExtended){ b = 1;}
+		if (hand.Fingers[2].IsExtended){ c = 1;}
+		if (hand.Fingers[3].IsExtended){ d = 1;}
+		if (hand.Fingers[4].IsExtended){ e = 1;}
+
+		toplam= a + b + c + d + e;
+
+		if (toplam == 1){ print(digits[0]);}
+		if (toplam == 2){ print(digits[1]);}
+		if (toplam == 3){ print(digits[2]);}
+		if (toplam == 4){ print(digits[3]);}
+		if (toplam == 5){ print(digits[4]);}
 		
 	}
 	
-*/	
+	
 	
 	
 	void Start ()
 	{
-
-		if (Other.Thumb == PointingState.Extended && Other.MaximumExtendedCount == 1)
-		{
-			print("1");
-		}
-		else
-		{
-			print("NO");
-		}
-		
-		
 	
 		
-	// Condition 02 
-
-		
-	//	Other.Thumb = PointingState.Extended;
-	//	Other.Index = PointingState.Extended;
-	//	Other.MaximumExtendedCount = 2;
-		
-		
-		
-		
-		
-		GetComponent<Renderer>().material.color = Color.yellow;
-
 		digits[0] = "1";
 		digits[1] = "2";
 		digits[2] = "3";
 		digits[3] = "4";
 		digits[4] = "5";
-
+		
+		GetComponent<Renderer>().material.color = Color.yellow;
+		
+		
 	//	GameObject.FindWithTag("Player").GetComponent<ExtendedFingerDetector>().Thumb = PointingState.Extended;
 
 
@@ -132,9 +148,6 @@ public class ReactionTimeCalc : MonoBehaviour {
 	void Update ()
 	{
 
-		
-		
-		
 		if (isTiming)
 		{
 			reactionTimer += Time.deltaTime;
@@ -148,7 +161,7 @@ public class ReactionTimeCalc : MonoBehaviour {
 			
 		}
 
-		
+	
 		
 		
 		
